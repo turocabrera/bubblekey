80e1344fe914203b424665403f2266b1636b043a
turocabrera <arturo.cabrera@ex-cle.com>
2026-01-31 23:36:02 -0300

    Se logro agregar el sprite del personaje. Sera un grafico momentaneo

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 import arcade
 import constants
+from arcade import Rect
+
+CHARACTER_SCALING = 4
 
 class BubblekeyGame(arcade.Window):
     def __init__(self):
-        super().__init__(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
-        
+        super().__init__(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)        
         # Listas para organizar nuestros sprites
         self.player_list = None
-        self.wall_list = None
-        
-        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+        self.wall_list = None        
+        # arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+        arcade.set_background_color(arcade.csscolor.BLUE_VIOLET)
 
     def setup(self):
-        """ Configuración inicial del juego (se llama al empezar o reiniciar) """
         self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList()
 
-        # Aquí es donde cargarías tu mapa de Tiled en el futuro:
-        # self.tile_map = arcade.load_tilemap(constants.MAPS_PATH / "nivel1.tmx")
+        full_texture = arcade.load_texture(str(constants.SPRITES_PATH / "spritesbubble.png"))
+
+        sub_texture = full_texture.crop(328, 15, 18, 18)
+
+        self.player_sprite = arcade.Sprite(sub_texture)
+        
+        self.player_sprite.scale = CHARACTER_SCALING
+        self.player_sprite.center_x = constants.SCREEN_WIDTH / 2
+        self.player_sprite.center_y = constants.SCREEN_HEIGHT / 2        
+        self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         """ Renderizar la pantalla """
-        self.clear()
-        # Aquí dibujaríamos las listas
-        # self.wall_list.draw()
-        # self.player_list.draw()
+        self.clear()        
+        self.player_list.draw()
 
 def main():
     window = BubblekeyGame()
